e367a7f7ee6c37658705b5a19a751843ffe07736
Javier Navarro Véliz <j.navarroveliz@gmail.com>
2025-07-06 22:03:32 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+import os
 from config import STATION_ID, API_BASE_URL, DB_PATH
 
-print(f"Station ID: {STATION_ID}")
-print(f"API Base URL: {API_BASE_URL}")
-print(f"Database Path: {DB_PATH}")
+# creating the db directory if it doesnt exist
+def create_db_directory():
+    os.makedirs(os.path.dirname(DB_PATH), exist_ok=True)
+
+def main():
+    #Given the small data volume (7 days), pandas is sufficient and efficient for this task.
+
 
